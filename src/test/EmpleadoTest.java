843eb0d9259863870f7d3fe4dd3c9f9755ee0d0d
usuario <usuario@pc21-101>
2023-10-03 17:33:20 +0200

    añadido cabecera test

---
+++ src/test/EmpleadoTest.java
@@ -0,0 +1,94 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EmpleadoTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testCalculaNominaEmpleadoEncargado() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculaNominaVendedor() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculaNominaOtro() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculaNominaVMnNegativa() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculaNominaVMmenor1000() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculaNominaVM1000_1500() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculaNominaVMMayor1500() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculaNominaHXPositivas() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculaNominaHXNegativa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCalculoNominaNetaNegativa() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculoNominaNetaMenos2100() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculoNominaNetaEntre2100_2500() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculoNominaNetaMayor2500() {
+		fail("Not yet implemented");
+	}
+
+}
